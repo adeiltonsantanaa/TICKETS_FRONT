62972ba34311ca7896a1c5caea1080326f52b510
Adeilton <filhoaguiar20@gmail.com>
2023-02-08 14:51:46 -0300

    feat: adicionando a rota de interação

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import Interacao from "./Pages/Interacao";
 
 
 
@@ -11,6 +12,7 @@ export default function rotas() {
             <Routes>
                 <Route path="/" element={<Login/>}/>
                 <Route path="/home" element={<Home/>}/>
+                <Route path="/interacao" element={<Interacao/>}/>
             </Routes>
         </BrowserRouter>
     )
