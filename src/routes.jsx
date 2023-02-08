6db3d65f97bf78d167cfaf665742083e4ecbc051
Adeilton <filhoaguiar20@gmail.com>
2023-02-07 21:14:27 -0300

    feat: adicionando rota home

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
+import Home from "./Pages/Home";
 
 
 
@@ -9,6 +10,7 @@ export default function rotas() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login/>}/>
+                <Route path="/home" element={<Home/>}/>
             </Routes>
         </BrowserRouter>
     )
