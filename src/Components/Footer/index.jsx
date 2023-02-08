204bb4f10158f08a9c6d7211846eec04b118eef0
Adeilton <filhoaguiar20@gmail.com>
2023-02-07 21:25:06 -0300

    feat: adicionando componente footer

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './style.css';
+
+
+
+export default function Footer(){
+
+    return(
+        <footer>
+            <h5>Desenvolvido por <a target="_blank" href="https://www.linkedin.com/in/adeiltonsantana/" rel="noreferrer">Adeilton Santana</a></h5>
+        </footer>
+    )
+}
