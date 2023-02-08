92027a1ea9be544f0a2225eac9fe9f1f283b7194
Adeilton <filhoaguiar20@gmail.com>
2023-02-08 14:54:47 -0300

    feat: ajustando a url de fechar - passando o cod do ticket para ser fechado no backend

--- src/Components/Fechar/index.jsx
+++ src/Components/Fechar/index.jsx
@@ -1,21 +1,19 @@
-import axios from "axios";
 import React from "react";
 import './style.css';
+import { FiCheck } from 'react-icons/fi'
+import axios from "axios";
 import { BASE_URL } from "../../utils/url";
-import {FiCheck} from 'react-icons/fi'
 
+export default function Fechar(props) {
 
-export default function Fechar(id) {
-
-    //configurar essa função de acordo com o endPoint
-    function fechar(id) {
-        axios.delete(`${BASE_URL}/id`)
+    function fechar() {
+        axios.put(`${BASE_URL}/api/ticket/${props.id}`)
     }
 
 
     return (
         <>
-            <button onClick={() => fechar}><FiCheck/></button>
+            <button className="btn-fechar-icons" onClick={fechar}><FiCheck /></button>
         </>
     )
 }
