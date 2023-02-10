7065796b9d55bb5ac79862041e001cdd9df2423e
Adeilton <filhoaguiar20@gmail.com>
2023-02-09 21:18:34 -0300

    feat: implementando envio de token

--- src/Pages/Interacao/index.jsx
+++ src/Pages/Interacao/index.jsx
@@ -3,7 +3,6 @@ import './style.css';
 import { useLocation } from "react-router-dom";
 import Header from "../../Components/Header";
 import Footer from "../../Components/Footer";
-import logo from '../../Assets/minhaFoto.jpeg'
 import { FiArrowLeft } from 'react-icons/fi'
 import axios from "axios";
 import { BASE_URL } from "../../utils/url";
@@ -15,26 +14,32 @@ export default function Interacao() {
     const location = useLocation();
     const [interacao, SetInteracao] = useState();
     const [ticket, SetTicket] = useState([]);
-    const [dadosTicket, setDadosTicket] = useState([]);
+    const [dadosTicket, setDadosTicket] = useState();
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/api/ticket/buscar/${location.state.id}`)
-            .then(res => { setDadosTicket(res.data); SetTicket(res.data.mensagens) })
+        const token = localStorage.getItem('accessToken');
+        axios.get(`${BASE_URL}/api/ticket/buscar/${location.state.id}`, { headers: { Authorization: 'Bearer ' + token } })
+            .then(res => { console.log(res.data); setDadosTicket(res.data); SetTicket(res.data.mensagens) })
             .catch(err => { console.log(err) })
-    }, []);
+    }, [location.state.id]);
 
     function atualizaMensagens(novoTicket) {
         SetTicket(ticket => [...ticket, novoTicket]);
     }
 
     function adicionarInteracao() {
+        const token = localStorage.getItem('accessToken');
+        const codFuncionario = localStorage.getItem('codFuncionario');
         axios.post(`${BASE_URL}/api/mensagem/salvar`, {
             codMensagem: null,
             descricao: interacao,
             ticket: {
                 codTicket: location.state.id
+            },
+            funcionario: {
+                codFuncionario: codFuncionario
             }
-        }).then(res => atualizaMensagens(res.data)).catch(err => console.log(err))
+        }, { headers: { Authorization: 'Bearer ' + token } }).then(res => atualizaMensagens(res.data)).catch(err => console.log(err))
     }
 
     return (
@@ -44,9 +49,9 @@ export default function Interacao() {
                 <div className="div-interacao-interna">
                     <h1>Ticket: <span className="span-interacao">123</span></h1>
                     <div className="div-interacao-cabecalho">
-                        <div className="div-interacao-assuntos">
-                            {dadosTicket && (
-                                <>
+                        {dadosTicket &&
+                            (
+                                <div key={dadosTicket.codTicket} className="div-interacao-assuntos">
                                     <div className="row">
                                         <h3>Id:</h3>
                                         <input value={dadosTicket.codTicket} className="input-interacao" type="text" disabled />
@@ -55,17 +60,16 @@ export default function Interacao() {
                                         <input value={dadosTicket.assunto} className="input-interacao" type="text" disabled />
                                     </div><div className="row">
                                         <h3>Funcionário:</h3>
-                                        <input value={"Adeilton MOCK"} className="input-interacao" type="text" disabled />
+                                        <input value={dadosTicket.funcionarios.nome} className="input-interacao" type="text" disabled />
                                     </div><div className="row">
                                         <h3>Nome do Problema:</h3>
-                                        <input value={dadosTicket.problemas[0].nome} className="input-interacao" type="text" disabled />
+                                        <input value={dadosTicket.problemas.nome} className="input-interacao" type="text" disabled />
                                     </div><div className="row">
-                                        <h3>Código do Problema:</h3>
-                                        <input value="5" className="input-interacao" type="text" disabled />
+                                        <h3>Setor:</h3>
+                                        <input value={dadosTicket.funcionarios.setor.nome} className="input-interacao" type="text" disabled />
                                     </div>
-                                </>
+                                </div>
                             )}
-                        </div>
                     </div>
                     <h2>Enviar Interação:</h2>
                     <div className="div-interacao-text">
@@ -82,7 +86,7 @@ export default function Interacao() {
                                         <textarea disabled value={msg.descricao} id="text-ticket" cols="178" rows="1"></textarea>
                                         <FiArrowLeft />
                                         <div className="div-interacao-circulo">
-                                            <img src={logo} alt="" />
+                                            <img src={`/fotos/${msg.funcionario.codFuncionario}.jpeg`} alt="" />
                                         </div>
                                     </div>
                                 </div>
