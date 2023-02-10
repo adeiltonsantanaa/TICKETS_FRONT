6fcde0a4ad696130f111e6986059a2c5eede2bf5
Adeilton <filhoaguiar20@gmail.com>
2023-02-10 19:53:49 -0300

    feat: alterando a localização do footer

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -31,28 +31,30 @@ export default function Login() {
     };
 
     return (
-        <div className="div-login-container">
-            <div className="div-login-box">
-                <div className="div-login-title">
-                    <h2>Sign in to <strong>Tickets Uni LS</strong></h2>
-                    <p>Sistema de realizações de chamados.</p>
-                </div>
-                <div className="div-login-inputs">
-                    <form onSubmit={login}>
-                        <div>
-                            <label htmlFor="usr-login-input">Username</label>
-                            <input id="usr-login-input" type="text" onChange={e => setUsername(e.target.value)} />
-                        </div>
-                        <div>
-                            <label htmlFor="usr-login-pwd">Password</label>
-                            <input id="usr-login-pwd" type="password" onChange={e => setPassword(e.target.value)} />
-                        </div>
-                        <span><a href="mailto:tecnologia@unils.edu.br?subject=Problemas para fazer login no tickets">problemas com o Login</a></span>
-                        <button className="btn-login" type="submit">Login</button>
-                    </form>
+        <>
+            <div className="div-login-container">
+                <div className="div-login-box">
+                    <div className="div-login-title">
+                        <h2>Sign in to <strong>Tickets Uni LS</strong></h2>
+                        <p>Sistema de realizações de chamados.</p>
+                    </div>
+                    <div className="div-login-inputs">
+                        <form onSubmit={login}>
+                            <div>
+                                <label htmlFor="usr-login-input">Username</label>
+                                <input id="usr-login-input" type="text" onChange={e => setUsername(e.target.value)} />
+                            </div>
+                            <div>
+                                <label htmlFor="usr-login-pwd">Password</label>
+                                <input id="usr-login-pwd" type="password" onChange={e => setPassword(e.target.value)} />
+                            </div>
+                            <span><a href="mailto:tecnologia@unils.edu.br?subject=Problemas para fazer login no tickets">problemas com o Login</a></span>
+                            <button className="btn-login" type="submit">Login</button>
+                        </form>
+                    </div>
+                    <Footer />
                 </div>
             </div>
-            <Footer />
-        </div>
+        </>
     )
 }
