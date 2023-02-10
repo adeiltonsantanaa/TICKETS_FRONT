7f56dd3b4570deaa1644954c304ecb705b61764a
Adeilton <filhoaguiar20@gmail.com>
2023-02-09 21:17:41 -0300

    feat: implementando mecanismo de login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../../Components/Footer";
 import './style.css';
+import axios from "axios";
+import { BASE_URL } from "../../utils/url";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const history = useNavigate();
+
+    async function login(e) {
+        e.preventDefault();
+        const data = {
+            username,
+            password,
+        };
+        try {
+            const response = await axios.post(`${BASE_URL}/auth/signin`, data);
+            localStorage.setItem('accessToken', response.data.accessToken);
+            localStorage.setItem('name', response.data.username)
+            localStorage.setItem('funcionario', response.data.nomeFuncionario)
+            localStorage.setItem('codFuncionario', response.data.codFuncionario)
+            history('/home')
+        } catch (err) {
+            alert("Login ou Senha incorreto, tente novamente!");
+        }
+
+    };
 
     return (
         <div className="div-login-container">
@@ -12,19 +38,21 @@ export default function Login() {
                     <p>Sistema de realizações de chamados.</p>
                 </div>
                 <div className="div-login-inputs">
-                    <div>
-                        <label htmlFor="usr-login-input">Username</label>
-                        <input id="usr-login-input" type="text" />
-                    </div>
-                    <div>
-                        <label htmlFor="usr-login-pwd">Password</label>
-                        <input id="usr-login-pwd" type="password" />
-                    </div>
-                    <span><a href="mailto:tecnologia@unils.edu.br?subject=Problemas para fazer login no tickets">problemas com o Login</a></span>
-                    <button className="btn-login">Login</button>
+                    <form onSubmit={login}>
+                        <div>
+                            <label htmlFor="usr-login-input">Username</label>
+                            <input id="usr-login-input" type="text" onChange={e => setUsername(e.target.value)} />
+                        </div>
+                        <div>
+                            <label htmlFor="usr-login-pwd">Password</label>
+                            <input id="usr-login-pwd" type="password" onChange={e => setPassword(e.target.value)} />
+                        </div>
+                        <span><a href="mailto:tecnologia@unils.edu.br?subject=Problemas para fazer login no tickets">problemas com o Login</a></span>
+                        <button className="btn-login" type="submit">Login</button>
+                    </form>
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
