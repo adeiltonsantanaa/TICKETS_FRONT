cb30db92aad7fa3fcff589ff5608a65eba613699
Adeilton Santana <filhoaguiar20@gmail.com>
2023-02-11 20:18:52 -0300

    feat: adicionando funcionalidas de divisao adm/fun - criando função para decidir qual endpoint consultar baseado na role - função de tratamento de nome primeira maiscula e resto minuscula

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -11,14 +11,30 @@ export default function Home() {
 
     const [open, setOpen] = useState(false);
     const [tickets, setTickets] = useState([]);
+    const nomeUsuario = ajustaNomeUsuario();
+
+    function ajustaNomeUsuario() {
+        var nomeMinusculo = localStorage.getItem('funcionario').toLowerCase()
+        return nomeMinusculo.charAt(0).toUpperCase().concat(nomeMinusculo.slice(1))
+    }
 
     function openClose(data) {
         setOpen(!open)
     }
 
+    function verificaRole() {
+        const role = localStorage.getItem('role')
+        const user = localStorage.getItem('codFuncionario');
+        if (role === "ADM") {
+            return "/api/ticket/todos/ativos"
+        }
+        return `/api/ticket/todos/ativos/${user}`
+    }
+
     useEffect(() => {
+        const request = verificaRole()
         const token = localStorage.getItem('accessToken');
-        axios.get(`${BASE_URL}/api/ticket/todos/ativos`, { headers: { Authorization: 'Bearer ' + token } })
+        axios.get(`${BASE_URL}${request}`, { headers: { Authorization: 'Bearer ' + token } })
             .then(res => { console.log(res.data); setTickets(res.data); setTickets(res.data) })
             .catch(err => { console.log(err) })
     }, []);
@@ -26,13 +42,13 @@ export default function Home() {
     return (
         <>
             {open && (
-                <ModalTicket openClose={openClose} />
+                <ModalTicket openClose={openClose} nome={nomeUsuario} />
             )}
             <Header />
             <div className="div-home-container">
                 <div className="div-home-interna">
                     <div className="div-home-add">
-                        <h1>Tickets Abertos: <strong>{tickets.length}</strong></h1>
+                        <h1>Olá, <span>{nomeUsuario}</span>.<br /> Você tem <strong>{tickets.length}</strong> tickets Abertos em seu nome.</h1>
                         <button onClick={openClose} >Add Ticket</button>
                     </div>
                     <table>
@@ -40,7 +56,7 @@ export default function Home() {
                             <tr>
                                 <th>ID</th>
                                 <th>ASSUNTO</th>
-                                <th>ÚLTIMA MENSAGEM</th>
+                                <th>FUNCIONÁRIO</th>
                                 <th>COD. PROBLEMA</th>
                                 <th>NOME PROBLEMA</th>
                                 <th>ENVIAR INTERAÇÃO</th>
@@ -52,7 +68,7 @@ export default function Home() {
                                 <tr key={t.codTicket}>
                                     <td>{t.codTicket}</td>
                                     <td>{t.assunto}</td>
-                                    <td>*EM MANUTENÇÃO*</td>
+                                    <td>{t.funcionarios.nome}</td>
                                     {/* <td>{tickets[t.codTicket - 1].mensagens.length === 0
                                         ? "*sem mensagem*"
                                         : tickets[t.codTicket - 1].mensagens[t.mensagens.length - 1].descricao.substring(0, 15) + "..."}</td> */}
