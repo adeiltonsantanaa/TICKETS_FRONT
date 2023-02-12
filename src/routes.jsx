d0dc9cfa8c39f6a33afa4fb70260e523a1c488fc
Adeilton Santana <filhoaguiar20@gmail.com>
2023-02-12 19:24:19 -0300

    feat: add bloqueio de rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,18 +1,35 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { isAuthenticated, whatsRole } from "./utils/auth";
+
+
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import HomeAdm from "./Pages/HomeAdm";
 import Interacao from "./Pages/Interacao";
 
 
+
+const Private = ({ Component }) => {
+    return isAuthenticated() ? <Component /> : <Navigate to="/" />
+};
+
+const Role = ({ Adm, Func }) => {
+    if (whatsRole()) {
+        return <Private Component={Adm} />
+    }
+    return <Private Component={Func} />
+}
+
+const Restricted = () => {}
 
 export default function rotas() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Login/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/interacao" element={<Interacao/>}/>
+                <Route path="/" element={<Login />} />
+                <Route path="/home" element={<Role Adm={HomeAdm} Func={Home} />} />
+                <Route path="/interacao" element={<Private Component={Interacao} />} />
             </Routes>
         </BrowserRouter>
     )
