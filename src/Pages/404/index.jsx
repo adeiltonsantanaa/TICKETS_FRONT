4d3c93360fb155d114ed61d8f78e7488d6b1c99a
Adeilton Santana <filhoaguiar20@gmail.com>
2023-02-12 21:13:19 -0300

    feat: criação de page para erro 404

---
+++ src/Pages/404/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css';
+
+
+
+export default function Erro404() {
+
+
+    return (
+        <>
+            <div className="container-404">
+                <h1>404</h1>
+            </div>
+        </>
+    )
+}
