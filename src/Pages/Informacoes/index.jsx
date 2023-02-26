3c72903bbf620570a64c36eb1fda3f0a9a831b53
Adeilton Santana <filhoaguiar20@gmail.com>
2023-02-26 20:56:49 -0300

    feat: criando um modal pra preview da img

--- src/Pages/Informacoes/index.jsx
+++ src/Pages/Informacoes/index.jsx
@@ -5,12 +5,14 @@ import { BASE_URL } from "../../utils/url";
 import axios from "axios";
 import { ToastContainer, toast } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
+import ModalImagem from "../../Components/ModalImagem";
+
 
 export default function Informacoes() {
 
     const [file, setFile] = useState(null);
     const [previewUrl, setPreviewUrl] = useState(null);
-    const [openPreview, setOpenPreview] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
@@ -25,9 +27,11 @@ export default function Informacoes() {
         reader.readAsDataURL(file);
     };
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    function handleOpenClose() {
+        previewUrl === null ? toast.warn("Selecione uma imagem para visualizá-la") : setOpen(!open);
+    }
 
+    const handleSubmit = () => {
         if (file) {
             const formData = new FormData();
             const newName = localStorage.getItem('codFuncionario').concat(".jpeg")
@@ -35,31 +39,29 @@ export default function Informacoes() {
 
             const token = localStorage.getItem('accessToken')
             axios.post(`${BASE_URL}/api/imagem/salvarImagem`, formData, { headers: { Authorization: 'Bearer ' + token } })
-                .then(res => toast.success(res.data))
+                .then(res => { toast.success(res.data) })
                 .catch(error => toast.error(error.response.data));
-               
         }
-
     };
 
     return (
         <>
-            <ToastContainer/>
+            <ToastContainer />
             <Header />
+            {open && <ModalImagem openClose={handleOpenClose} image={previewUrl} />}
             <div className="div-informacoes-container">
                 <div className="div-informacoes-interna">
                     <div className="div-informacoes-box">
                         <div className="div-informacoes-descricao">
                             <h1>Olá, <span>{localStorage.getItem('funcionario')}</span>. Para alterar sua foto selecione o ícone abaixo e escolha o arquivo de imagem.</h1>
-                            <h2>Obs: O arquivo precisa ser jpeg e conter menos de 10Kb</h2>
+                            <h2>Obs: O arquivo precisa ser jpeg e conter menos de 10MB</h2>
                         </div>
                         <div className="div-informacoes-campos">
-                            {openPreview && <img src={previewUrl} alt="Preview" />}
-                            <form className="form-informacoes" onSubmit={handleSubmit}>
+                            <div className="div-informacoes-inputs">
                                 <input type="file" id="input-informacoes" onChange={handleFileChange} />
-                                <button className="btn-informacoes-ver" type="button" onClick={() => setOpenPreview(!openPreview)}>Ver Imagem</button>
-                                <button className="btn-informacoes-submit" type="submit">Salvar Imagem</button>
-                            </form>
+                                <button className="btn-informacoes-ver" type="button" onClick={handleOpenClose}>Ver Imagem</button>
+                                <button className="btn-informacoes-submit" onClick={handleSubmit} type="button">Salvar Imagem</button>
+                            </div>
                         </div>
                     </div>
                 </div>
