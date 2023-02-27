25001762d1b2ab97a0b96ff00f70f3238c20ef06
Adeilton Santana <filhoaguiar20@gmail.com>
2023-02-26 21:47:51 -0300

    feat: add rota única para a home

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import { isAuthenticated, whatsRole } from "./utils/auth";
+import { isAuthenticated } from "./utils/auth";
 
 
 import Login from "./Pages/Login";
-import Home from "./Pages/Home";
 import HomeAdm from "./Pages/HomeAdm";
 import Interacao from "./Pages/Interacao";
 import TicketsClosed from "./Pages/TicketsClosed";
@@ -16,19 +15,12 @@ const Private = ({ Component }) => {
     return isAuthenticated() ? <Component /> : <Navigate to="/" />
 };
 
-const Role = ({ Adm, Func }) => {
-    if (whatsRole()) {
-        return <Private Component={Adm} />
-    }
-    return <Private Component={Func} />
-}
-
 export default function rotas() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Role Adm={HomeAdm} Func={Home} />} />
+                <Route path="/home" element={<Private Component={HomeAdm} />} />
                 <Route path="/interacao" element={<Private Component={Interacao} />} />
                 <Route path="/home/ticketsFechados" element={<Private Component={TicketsClosed} />} />
                 <Route path="/informacoes" element={<Private Component={Informacoes} />} />
